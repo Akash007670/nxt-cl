a36b33ead02a4b0bbde98e8829cadacbd599bc9e
Akash kumar <akaysharma894@gmail.com>
2024-02-02 17:43:12 +0530

    Added changes for button and other refactoring'

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-
+import "./button.module.scss";
 export interface IButtonProps {
   label: string;
   cn?: string;
+  type?: "button" | "submit" | "reset";
+  handleClick?: () => void;
 }
 
 const Button = (props: IButtonProps) => {
-  const { label, cn } = props;
-  return <button className={`${cn}`}>{label}</button>;
+  const { label, cn, type, handleClick } = props;
+  const buttonClass = cn ? `button ${cn}` : "button";
+
+  return (
+    <button className={buttonClass} type={type} onClick={handleClick}>
+      {label && <span>{label}</span>}
+    </button>
+  );
 };
 
 export default Button;
