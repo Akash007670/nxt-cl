32716147dc4790570f11a80d1fce0a562ebbc4b9
Akash kumar <akaysharma894@gmail.com>
2024-02-04 00:17:30 +0530

    Added storybook along with button stories

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,47 @@
 import React from "react";
-import "./button.module.scss";
+import "./button.scss";
 export interface IButtonProps {
   label: string;
   cn?: string;
   type?: "button" | "submit" | "reset";
-  handleClick?: () => void;
+  size?: "sm" | "md" | "lg" | "xl";
+  variants?:
+    | "primary"
+    | "secondary"
+    | "ghost"
+    | "success"
+    | "danger"
+    | "info"
+    | "outlined";
+  color?: string;
+  iconLeft?: React.JSX.Element;
+  iconRight?: React.JSX.Element;
 }
 
-const Button = (props: IButtonProps) => {
-  const { label, cn, type, handleClick } = props;
-  const buttonClass = cn ? `button ${cn}` : "button";
-
+const Button = ({
+  label,
+  cn,
+  type = "button",
+  size = "md",
+  variants,
+  color,
+  iconLeft,
+  iconRight,
+  ...rest
+}: IButtonProps) => {
   return (
-    <button className={buttonClass} type={type} onClick={handleClick}>
+    <button
+      className={`button btn-${size} ${variants} ${cn}`}
+      type={type}
+      {...rest}
+      style={{
+        color: color ? color || "black" : "",
+        border: variants === "outlined" ? `2px solid ${color || "black"}` : "",
+      }}
+    >
+      {iconLeft && <span>{iconLeft}</span>}
       {label && <span>{label}</span>}
+      {iconRight && <span>{iconRight}</span>}
     </button>
   );
 };
