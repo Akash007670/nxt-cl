4e84866ed53b5dc5347c91bee4a7a571e271582c
Akash kumar <akaysharma894@gmail.com>
2024-10-02 01:27:41 +0530

    fix: fixed dropdown path in storybook

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 // import { Icons } from "@storybook/components";
-import { Dropdown, Option } from "./Dropdown";
+import Dropdown, { Option } from "./Dropdown";
 
 const meta = {
   title: "Nxt-cl/Dropdown",
